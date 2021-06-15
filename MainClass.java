21b0db2a2d6b2cdbf1ac9e0c1ea546f45dd5690f
unknown <madhuchowdary150@gmail.com>
2021-06-15 20:20:43 +0530

    second

---
+++ MainClass.java
@@ -0,0 +1,9 @@
+package com.revature.utils;
+import java.util.*;
+public class MainClass{
+    public static void main(String args[]){
+        Date today=new Date();
+        System.out.println("Date is"+today);
+    }
+}
+
---
+++ string.java
@@ -0,0 +1,19 @@
+public class string
+{
+    public static void main(String args[])
+    {
+        String str1="string";
+        String str2="string";
+        if(str1.equals(str2))
+        {
+            System.out.println("Equal");
+        
+        }
+        else
+        {
+            System.out.println("NOt equal");
+        }
+    }
+    
+
+}
